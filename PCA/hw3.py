6bc1d8e7b1b2f0c3c35cc8764a89309e3390c73f
hoonkiyeo <hky1721@gmail.com>
2022-02-08 19:56:53 -0600

    modfy probability and create pca folder and upload hw3 files

---
+++ PCA/hw3.py
@@ -0,0 +1,27 @@
+from scipy.linalg import eigh
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_and_center_dataset(filename):
+    # Your implementation goes here!
+    pass
+
+def get_covariance(dataset):
+    # Your implementation goes here!
+    pass
+
+def get_eig(S, m):
+    # Your implementation goes here!
+    pass
+
+def get_eig_prop(S, prop):
+    # Your implementation goes here!
+    pass
+
+def project_image(image, U):
+    # Your implementation goes here!
+    pass
+
+def display_image(orig, proj):
+    # Your implementation goes here!
+    pass
