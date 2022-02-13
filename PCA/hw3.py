ab85457d9bba024188311f971aae5ef70dd1e8ee
hoonkiyeo <hky1721@gmail.com>
2022-02-12 20:28:38 -0600

    upload debugging.ipynb and modify hw3.py

--- PCA/hw3.py
+++ PCA/hw3.py
@@ -3,12 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def load_and_center_dataset(filename):
-    # Your implementation goes here!
-    pass
+    x = np.load(filename)
+    n = len(x)
+    d = len(x[0])
+    recenter = x - np.mean(x, axis=0)
+    return recenter
 
 def get_covariance(dataset):
-    # Your implementation goes here!
-    pass
+    trans_data = np.transpose(dataset)
+    return (1 / (len(dataset) - 1)) * (trans_data@dataset)
 
 def get_eig(S, m):
     # Your implementation goes here!
@@ -25,3 +28,5 @@ def project_image(image, U):
 def display_image(orig, proj):
     # Your implementation goes here!
     pass
+
+print(load_and_center_dataset("YaleB_32x32.npy"))
