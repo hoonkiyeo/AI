c484dcd4aff43351c4ae6b36f1818d76838f5608
hoonkiyeo <hky1721@gmail.com>
2022-03-21 23:12:33 -0500

    upload NeuralNetwork folder and corresponding files

---
+++ NeuralNetwork/hw6/intro_pytorch.py
@@ -0,0 +1,94 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+# Feel free to import other packages, if needed.
+# As long as they are supported by CSL machines.
+
+
+def get_data_loader(training = True):
+    """
+    TODO: implement this function.
+
+    INPUT: 
+        An optional boolean argument (default value is True for training dataset)
+
+    RETURNS:
+        Dataloader for the training set (if training = True) or the test set (if training = False)
+    """
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+        ])
+
+
+
+def build_model():
+    """
+    TODO: implement this function.
+
+    INPUT: 
+        None
+
+    RETURNS:
+        An untrained neural network model
+    """
+
+
+
+
+def train_model(model, train_loader, criterion, T):
+    """
+    TODO: implement this function.
+
+    INPUT: 
+        model - the model produced by the previous function
+        train_loader  - the train DataLoader produced by the first function
+        criterion   - cross-entropy 
+        T - number of epochs for training
+
+    RETURNS:
+        None
+    """
+    
+
+
+def evaluate_model(model, test_loader, criterion, show_loss = True):
+    """
+    TODO: implement this function.
+
+    INPUT: 
+        model - the the trained model produced by the previous function
+        test_loader    - the test DataLoader
+        criterion   - cropy-entropy 
+
+    RETURNS:
+        None
+    """
+    
+
+
+def predict_label(model, test_images, index):
+    """
+    TODO: implement this function.
+
+    INPUT: 
+        model - the trained model
+        test_images   -  test image set of shape Nx1x28x28
+        index   -  specific index  i of the image to be tested: 0 <= i <= N - 1
+
+
+    RETURNS:
+        None
+    """
+
+
+if __name__ == '__main__':
+    '''
+    Feel free to write your own test code here to exaime the correctness of your functions. 
+    Note that this part will not be graded.
+    '''
+    criterion = nn.CrossEntropyLoss()
